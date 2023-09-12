027593868c8705dd29cdd75dd0ace7311a9259d5
thmylonas <thmylonas@hotmail.com>
2023-09-12 19:53:28 +0200

    SiiTransactionManagementApp: First push of the project

--- TransactionMicroserviceApp/src/test/java/com/thomasmylonas/transaction_microservice_app/TransactionMicroserviceAppApplicationTests.java
+++ TransactionMicroserviceApp/src/test/java/com/thomasmylonas/transaction_microservice_app/TransactionMicroserviceAppApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TransactionMicroserviceAppApplicationTests {
+public class TransactionMicroserviceAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
