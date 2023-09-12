bcbc86ec03a58a1e03190944359abcf2ba6da356
thmylonas <thmylonas@hotmail.com>
2023-09-12 20:12:47 +0200

    SiiTransactionManagementApp: First push of the project

--- src/main/java/com/thomasmylonas/sii_transaction_management_app/SiiTransactionManagementAppApplication.java
+++ src/main/java/com/thomasmylonas/sii_transaction_management_app/SiiTransactionManagementAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SiiTransactionManagementAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SiiTransactionManagementAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SiiTransactionManagementAppApplication.class, args);
+    }
 }
--- src/test/java/com/thomasmylonas/sii_transaction_management_app/SiiTransactionManagementAppApplicationTests.java
+++ src/test/java/com/thomasmylonas/sii_transaction_management_app/SiiTransactionManagementAppApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SiiTransactionManagementAppApplicationTests {
+public class SiiTransactionManagementAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
